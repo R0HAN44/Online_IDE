49dc09a71d2d8d3397d341902cc2a2e8ec1ca762
R0HAN44 <rohanj0306@gmail.com>
2023-09-20 19:31:38 +0530

    Initial commit

---
+++ JS-Editor/script.js
@@ -0,0 +1,80 @@
+let htmleditor = CodeMirror.fromTextArea(document.getElementById("html-code"), {
+  mode: "text/html",
+  theme: "dracula",
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseBrackets: true,
+  matchBrackets: true,
+  autoCloseTags: true,
+  closeTag: true,
+  matchTags: true,
+  keyMap: "sublime",
+  scrollbarStyle: "overlay",
+});
+htmleditor.setSize("100%", "28%");
+
+let csseditor = CodeMirror.fromTextArea(document.getElementById("css-code"), {
+  mode: "text/css",
+  theme: "dracula",
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseBrackets: true,
+  matchBrackets: true,
+  autoCloseTags: true,
+  closeTag: true,
+  matchTags: true,
+  keyMap: "sublime",
+  scrollbarStyle: "overlay",
+});
+csseditor.setSize("100%", "28%");
+
+let jseditor = CodeMirror.fromTextArea(document.getElementById("js-code"), {
+  mode: "text/javascript",
+  theme: "dracula",
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseBrackets: true,
+  matchBrackets: true,
+  autoCloseTags: true,
+  closeTag: true,
+  matchTags: true,
+  keyMap: "sublime",
+  scrollbarStyle: "overlay",
+});
+
+jseditor.setSize("100%", "28%");
+
+htmleditor.getDoc().setValue(localStorage.getItem("htmlcode"));
+csseditor.getDoc().setValue(localStorage.getItem("csscode"));
+jseditor.getDoc().setValue(localStorage.getItem("jscode"));
+
+htmleditor.on("change", function () {
+  run();
+});
+csseditor.on("change", function () {
+  run();
+});
+jseditor.on("change", function () {
+  run();
+});
+
+let themes = document.getElementById("theme");
+themes.addEventListener("change", () => {
+  htmleditor.setOption("theme", themes.value);
+  csseditor.setOption("theme", themes.value);
+  jseditor.setOption("theme", themes.value);
+});
+
+function run() {
+  let htmlCode = htmleditor.getValue();
+  localStorage.setItem("htmlcode", htmlCode);
+  let cssCode = csseditor.getValue();
+  localStorage.setItem("csscode", cssCode);
+  let jsCode = jseditor.getValue();
+  localStorage.setItem("jscode", jsCode);
+  let output = document.getElementById("output");
+  output.contentDocument.body.innerHTML =
+    htmlCode + `<style>${cssCode}</style>`;
+  output.contentWindow.eval(jsCode);
+}
+run();
---
+++ script.js
@@ -0,0 +1,127 @@
+const runButton = document.getElementById("runButton");
+const inputArea = document.getElementById("input");
+const outputArea = document.getElementById("output");
+
+let input = "";
+let code = "";
+let time_used;
+let memory_used;
+let status_detail;
+let output_url;
+let language = "C";
+let delay = 4000;
+const url =
+  "https://api.hackerearth.com/v4/partner/code-evaluation/submissions/";
+const compileCode = async () => {
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "client-secret": "61a02a00f6be54a02a0bebbe3c392e5c6c921691",
+    },
+    body: JSON.stringify({
+      lang: language,
+      source: code,
+      input: input,
+      memory_limit: 243232,
+      time_limit: 5,
+      callback: "https://client.com/callback/execute/result/",
+    }),
+  };
+
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  const status_url = data.status_update_url;
+  const client_secret = "61a02a00f6be54a02a0bebbe3c392e5c6c921691";
+  const headers = {
+    "client-secret": client_secret,
+  };
+  let response2 = await fetch(status_url, { headers });
+  let ans = await response2.json();
+  let interval = setInterval(async () => {
+    response2 = await fetch(status_url, { headers });
+    ans = await response2.json();
+
+    if (ans.request_status.code === "REQUEST_COMPLETED") {
+      time_used = ans.result.run_status.time_used;
+      memory_used = ans.result.run_status.memory_used;
+      status_detail = ans.result.run_status.status;
+      output_url = ans.result.run_status.output;
+      displayOutput();
+      clearInterval(interval);
+    } else if (ans.request_status.code === "REQUEST_FAILED") {
+      clearInterval(interval);
+    }
+  }, 3000);
+};
+
+const displayOutput = () => {
+  if (status_detail == "AC") {
+    document.getElementById("status_detail").innerText =
+      "Accepted. The code executed successfully.";
+  } else if (status_detail == "MLE") {
+    document.getElementById("status_detail").innerText =
+      "Memory Limit Exceeded. Execution of the compiled code used more memory that the memory limit";
+  } else if (status_detail == "TLE") {
+    document.getElementById("status_detail").innerText =
+      "Time Limit Exceeded. Execution of the compiled code took more time than the passed time limit";
+  } else if (status_detail == "RE") {
+    document.getElementById("status_detail").innerText =
+      "Invalid memory reference or segmentation fault";
+  }
+
+  document.getElementById("time_used").innerText = `Time Used : ${time_used} s`;
+  document.getElementById(
+    "memory_used"
+  ).innerText = `Memory Used : ${memory_used} KB`;
+  document.getElementById("download_button").setAttribute("href", output_url);
+};
+
+runButton.addEventListener("click", () => {
+  code = editor.getValue();
+  input = inputArea.value.toString();
+  if (code == "") {
+    alert("Enter the code");
+  } else {
+    document.getElementById("status_detail").innerText = "Executing Code....";
+    document.getElementById("time_used").innerText = "";
+    document.getElementById("memory_used").innerText = "";
+    compileCode();
+  }
+});
+
+var editor = CodeMirror.fromTextArea(document.getElementById("editor"), {
+  mode: "text/x-c++src",
+  theme: "3024-night",
+  lineNumbers: true,
+  lineWrapping: true,
+  autoCloseBrackets: true,
+  matchBrackets: true,
+  keyMap: "sublime",
+  extraKeys: { "Ctrl-Space": "autocomplete" },
+  scrollbarStyle: "overlay",
+});
+editor.setSize("70vw", "80vh");
+
+var option = document.getElementById("Language");
+option.addEventListener("change", () => {
+  if (option.value == "java") {
+    editor.setOption("mode", "text/x-java");
+    language = "JAVA14";
+  } else if (option.value == "python") {
+    editor.setOption("mode", "text/x-python");
+    language = "PYTHON3";
+  } else if (option.value == "cpp") {
+    editor.setOption("mode", "text/x-c++src");
+    language = "CPP17";
+  } else {
+    editor.setOption("mode", "text/x-c++src");
+    language = "C";
+  }
+});
+
+let themes = document.getElementById("theme");
+themes.addEventListener("change", () => {
+  editor.setOption("theme", themes.value);
+});
